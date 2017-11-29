7dec6b62a972fad0e14d18a590dac06c06697a1b
yodablair <rogerblair@gmail.com>
2017-11-29 11:14:36 +0000

    Add project files.

---
+++ CalendarYearMonth.Test/UnitTest1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalendarYearMonth.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddMonthTest1()
+        {
+            int year = 2016;
+            int month = 12;
+            int monthsToAdd = 3;
+
+            CalendarYearMonth calYrMth = new CalendarYearMonth(year, month);
+            CalendarYearMonth calYrMthResult = new CalendarYearMonth();
+            calYrMthResult = calYrMth + monthsToAdd;
+
+            Assert.AreEqual(3, calYrMthResult.Month);
+            Assert.AreEqual(2017, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddMonthTest2()
+        {
+            int year = 2016;
+            int month = 12;
+            int monthsToAdd = 20;
+
+            CalendarYearMonth calYrMth = new CalendarYearMonth(year, month);
+            CalendarYearMonth calYrMthResult = new CalendarYearMonth();
+            calYrMthResult = calYrMth + monthsToAdd;
+
+            Assert.AreEqual(8, calYrMthResult.Month);
+            Assert.AreEqual(2018, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddMonthTest3()
+        {
+            int year = 2016;
+            int month = 7;
+            int monthsToAdd = 4;
+
+            CalendarYearMonth calYrMth = new CalendarYearMonth(year, month);
+            CalendarYearMonth calYrMthResult = new CalendarYearMonth();
+            calYrMthResult = calYrMth + monthsToAdd;
+
+            Assert.AreEqual(11, calYrMthResult.Month);
+            Assert.AreEqual(2016, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddMonthTest4()
+        {
+            int year = 2016;
+            int month = 8;
+            int monthsToAdd = 4;
+
+            CalendarYearMonth calYrMth = new CalendarYearMonth(year, month);
+            CalendarYearMonth calYrMthResult = new CalendarYearMonth();
+            calYrMthResult = calYrMth + monthsToAdd;
+
+            Assert.AreEqual(12, calYrMthResult.Month);
+            Assert.AreEqual(2016, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddMonthTest5()
+        {
+            int year = 2016;
+            int month = 8;
+            int monthsToAdd = 16;
+
+            CalendarYearMonth calYrMth = new CalendarYearMonth(year, month);
+            CalendarYearMonth calYrMthResult = new CalendarYearMonth();
+            calYrMthResult = calYrMth + monthsToAdd;
+
+            Assert.AreEqual(12, calYrMthResult.Month);
+            Assert.AreEqual(2017, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddCalendarYearMonthTest1()
+        {
+            int year1 = 2016; int month1 = 12;
+            int year2 = 3; int month2 = 4;
+
+            CalendarYearMonth calYrMth1 = new CalendarYearMonth(year1, month1);
+            CalendarYearMonth calYrMth2 = new CalendarYearMonth(year2, month2);
+            CalendarYearMonth calYrMthResult = calYrMth1 + calYrMth2;
+
+            Assert.AreEqual(4, calYrMthResult.Month);
+            Assert.AreEqual(2020, calYrMthResult.Year);
+        }
+
+        [TestMethod]
+        public void AddCalendarYearMonthTest2()
+        {
+            int year1 = 2016; int month1 = 8;
+            int year2 = 2; int month2 = 16;
+
+            CalendarYearMonth calYrMth1 = new CalendarYearMonth(year1, month1);
+            CalendarYearMonth calYrMth2 = new CalendarYearMonth(year2, month2);
+            CalendarYearMonth calYrMthResult = calYrMth1 + calYrMth2;
+
+            Assert.AreEqual(12, calYrMthResult.Month);
+            Assert.AreEqual(2019, calYrMthResult.Year);
+        }
+
+    }
+
+}
---
+++ CalendarYearMonth/CalendarYearMonth.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarYearMonth
+{
+    public class CalendarYearMonth
+    {
+        #region Constructors
+
+        public CalendarYearMonth() {}
+
+        public CalendarYearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        #endregion
+
+        #region Member variables
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        #endregion
+
+        public static CalendarYearMonth operator +(CalendarYearMonth calendarYearMonth, int months)
+        {
+            CalendarYearMonth calYrMth;
+            int totalMonths = ConvertCalendarYearMonthToMonths(calendarYearMonth) + months;
+            calYrMth = ConvertMonthsToCalendarYearMonth(totalMonths);
+            return calYrMth;
+        }
+
+        public static CalendarYearMonth operator +(CalendarYearMonth calendarYearMonth1, CalendarYearMonth calendarYearMonth2)
+        {
+            CalendarYearMonth calYrMth;
+            int totalMonths = ConvertCalendarYearMonthToMonths(calendarYearMonth1)
+                + ConvertCalendarYearMonthToMonths(calendarYearMonth2);
+            calYrMth = ConvertMonthsToCalendarYearMonth(totalMonths);
+            return calYrMth;
+        }
+
+        #region Private functions
+
+        private static int ConvertCalendarYearMonthToMonths(CalendarYearMonth calYrMth)
+        {
+            return (calYrMth.Year * 12) + calYrMth.Month;
+        }
+
+        private static CalendarYearMonth ConvertMonthsToCalendarYearMonth(int months)
+        {
+            CalendarYearMonth calYrMth = new CalendarYearMonth();
+            int modMonth = months % 12;
+            if (modMonth == 0)
+            {
+                calYrMth.Year = (months / 12) - 1;
+                calYrMth.Month = 12;
+            }
+            else
+            {
+                int year = (int)Math.Truncate((decimal)(months / 12));
+                calYrMth.Year = year;
+                calYrMth.Month = modMonth;
+            }
+            return calYrMth;
+        }
+
+        #endregion
+    }
+}
